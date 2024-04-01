7160adb1ee1c549983e19a3f6787a7dd592c9242
wwwriterrr <royal.writer@yandex.ru>
2024-04-01 00:33:57 -0600

    Create directory and files

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Handlebars from 'handlebars';
+import * as Components from './components';
+import * as Pages from './pages';
+
+
+const pages = {
+  'login': [ Pages.LoginPage, {test: '123'} ],
+  'list': [ Pages.ListPage ],
+  'nav': [ Pages.NavigatePage ]
+};
+
+Object.entries(Components).forEach(([ name, component ]) => {
+  Handlebars.registerPartial(name, component);
+});
+
+function navigate(page) {
+  //@ts-ignore
+  const [ source, context ] = pages[page];
+  const container = document.getElementById('app');
+  container.innerHTML = Handlebars.compile(source)(context);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('nav'));
+
+document.addEventListener('click', e => {
+  //@ts-ignore
+  const page = e.target.getAttribute('page');
+  if (page) {
+    navigate(page);
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+});
--- src/pages/nav/index.js
+++ src/pages/nav/index.js
@@ -1,1 +1,1 @@
-export { default as NavigatePage } from './navigate.hbs?raw';
+export { default as NavigatePage } from './nav.hbs?raw';
