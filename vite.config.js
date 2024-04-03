b992409db50fa4f207618e277c7edaba76b610ff
wwwriterrr <royal.writer@yandex.ru>
2024-04-03 06:40:47 -0600

    Files change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     root: './',
     //base: './',
-    publicDir: './',
+    publicDir: './src/assets/',
     build: {
         outDir: './dist/',
         port: 3000
