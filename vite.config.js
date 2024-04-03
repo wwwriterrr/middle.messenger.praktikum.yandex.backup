56bbd41d299bc3926c4364257b943f78eea4ea1d
wwwriterrr <royal.writer@yandex.ru>
2024-04-03 06:06:33 -0600

    Vite config changed

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: '.',
+    root: './',
     base: './',
     publicDir: './src/assets/',
     build: {
-        outDir: 'dist/',
+        outDir: './dist/',
         port: 3000
     },
     server: {
         host: '0.0.0.0',
-        outDir: 'dist/',
+        outDir: './dist/',
         port: 3000,
         minify: 'esbuild',
     },
