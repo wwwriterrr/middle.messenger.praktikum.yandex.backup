e55579f1352c243e1e1685d415060d3d2b0cbe9f
wwwriterrr <royal.writer@yandex.ru>
2024-04-03 21:47:09 +0100

    Files changes and add postcss preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 import gulp from 'gulp';
 import postcss from 'gulp-postcss';
 
+import autoprefixer from 'autoprefixer';
+import cssnext from 'cssnext';
+import precss from 'precss';
+
 gulp.task('css', function () {
-    const processors = [];
+    const processors = [autoprefixer, cssnext, precss];
     return gulp.src('./src/*.css')
         .pipe(postcss(processors))
         .pipe(gulp.dest('./dist'));
