2602b09cec7f124756dacf5a5d867d05c081afba
wwwriterrr <royal.writer@yandex.ru>
2024-04-03 00:44:02 -0600

    Files changes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: '.',
+    base: './',
+    publicDir: './src/assets/',
     build: {
         outDir: 'dist',
         port: 3000
     },
     server: {
+        host: '0.0.0.0',
         outDir: 'dist',
-        port: 3000
+        port: 3000,
+        minify: 'esbuild',
     },
 })
